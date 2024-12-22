dc0f3621b53118ebfc6fba4addde80b0df2c26c7
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-12-22 23:22:35 +0530

    init

---
+++ app/actions/analyze.ts
@@ -0,0 +1,23 @@
+"use server";
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+export async function analyze(code: string) {
+  const prompt = `You are given with a code : ${code}. Your job is to analyze time and space complexity
+                    of the code accurately. 
+                    IMPORTANT: Return time complexity, explanation for time complexity, space complexity, explanation for space complexity at once seperated by |
+                    IMPORTANT: Do not provide any extra or unnecessary details/information
+                    IMPORTANT: Response should be in normal text. Don't bold italic etc
+                    IMPORTANT: If something other than code is given. Return invalid for all params`;
+
+  const result = await model.generateContent([prompt]);
+  const [time, timeExplanation, space, spaceExplanation] = result.response
+    .text()
+    .split("|");
+
+  return { time, timeExplanation, space, spaceExplanation };
+}
