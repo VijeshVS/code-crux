bea2e1a54393719207fbc4da81e9b6ef04c59b21
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-12-22 23:24:26 +0530

    fix

--- app/actions/analyze.ts
+++ app/actions/analyze.ts
@@ -1,8 +1,8 @@
 "use server";
 
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.API_KEY || "");
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
