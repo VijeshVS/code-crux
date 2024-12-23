adae55055b16b46f2106bcc48150e9c5d3bc7fac
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-12-23 18:34:42 +0530

    promt

--- app/actions/analyze.ts
+++ app/actions/analyze.ts
@@ -40,6 +40,7 @@ export async function getDetails(code: string) {
                       }[],
                       final_space_complexity_explanation: string
                     }
+                    Please analyze the code carefully and come to conclusion. Don't give the time or space complexity of optimized/better code. Just give results for current code given to you.
                     IMPORTANT: Don't suggest the ways to optimize code. Just be concerned with the code given to you and analyze it.
                     IMPORTANT: Do not provide any extra or unnecessary details/information
                     IMPORTANT: Response should be in normal text. Don't bold italic etc
